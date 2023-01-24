3b63b22c461aa2f06034436e3563e8892baf1a8a
Hossam Salah <hossamsalahabbas@gmail.com>
2023-01-24 10:19:38 +0200

    Added first Three Labs

--- D03/Task01.java
+++ D03/Task01.java
@@ -1,16 +1,22 @@
 import java.util.function.Function;
 
+class ConvertToFahrenheit implements Function<Double, Double> {
+    public Double apply(Double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+}
+
 class Task01 {
-    public static int TempConvert(int celsius, Function<Integer, Integer> convertToFahrenheit) {
-        return convertToFahrenheit.apply(celsius);
+    public static Double TempConvert(double celsius, Function<Double, Double> convertToFahr) {
+        return convertToFahr.apply(celsius);
     }
 
     public static void main(String args[]) {
         if (args.length != 1) {
             System.out.println("You Must Provide a Temprature to convert");
         } else {
-            Function<Integer, Integer> convertToFahrenheit = (celsius) -> (celsius * 9 / 5) + 32;
-            int fehr = TempConvert(Integer.parseInt(args[0]), convertToFahrenheit);
+            Function<Double, Double> convertToFahrenheit = new ConvertToFahrenheit();
+            Double fehr = TempConvert(Integer.parseInt(args[0]), convertToFahrenheit);
             System.out.println("Temp " + args[0] + "C = " + fehr + "F");
         }
     }
