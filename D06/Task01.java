698cd0054ca4b2a07341694089ddd25c6d1aaf63
Hossam Salah <hossamsalahabbas@gmail.com>
2023-01-28 10:09:59 +0200

    Update Task01 D06

--- D06/Task01.java
+++ D06/Task01.java
@@ -2,45 +2,53 @@ import java.util.*;
 
 class Task01 {
 
-    static Map<Integer, ArrayList<String>> dictionary;
+    static Map<Character, ArrayList<String>> dictionary = new HashMap<>();
 
-    public Task01() {
-        dictionary = new HashMap<>();
-        for (int i = 0; i < 26; i++) {
-            dictionary.put(i + 97, new ArrayList<String>());
-        }
-    }
-
-    public void storeWords(String newWord) {
-        int firstChar = Character.toLowerCase(newWord.charAt(0));
-        ArrayList<String> tmp = dictionary.get(firstChar);
-        tmp.add(newWord);
-        Collections.sort(tmp);
-        dictionary.put(firstChar, tmp);
+    public void storeWord(String word) {
+        char firstChar = Character.toLowerCase(word.charAt(0));
+        if (dictionary.get(firstChar) == null) {
+            ArrayList<String> wordList = new ArrayList<>();
+            wordList.add(word);
+            Collections.sort(wordList);
+            dictionary.put(firstChar, wordList);
+        } else {
+            ArrayList<String> wordList = dictionary.get(firstChar);
+            wordList.add(word);
+            Collections.sort(wordList);
+            dictionary.put(firstChar, wordList);
+        } // Check if char allready Exists
     }
 
     public void printAllDictionary() {
-        for (Map.Entry<Integer, ArrayList<String>> set : dictionary.entrySet()) {
-            char ch = (char) set.getKey().intValue();
-            System.out.println(ch + ":" + set.getValue());
+        for (Map.Entry<Character, ArrayList<String>> set : dictionary.entrySet()) {
+            System.out.println(set.getKey() + ":" + set.getValue());
         }
     }
 
     public void printSpecificChar(Character character) {
-        int charKey = Character.toLowerCase(character);
-        ArrayList<String> list = dictionary.get(charKey);
-        char ch = (char) character;
-        System.out.println(ch + ":" + list);
+        ArrayList<String> list = dictionary.get(character);
+        System.out.println(character + ":" + list);
     }
 
     public static void main(String args[]) {
         Task01 dict = new Task01();
+        dict.storeWord("hossam");
+        dict.storeWord("Nader");
+        dict.storeWord("Ahmed");
+        dict.storeWord("ali");
+        dict.storeWord("mahmoud");
+        dict.storeWord("ramy");
+        dict.storeWord("basem");
+
         if (args.length > 0) {
             for (int i = 0; i < args.length; i++) {
-                dict.storeWords(args[i]);
+                dict.storeWord(args[i]);
             }
             dict.printAllDictionary();
+            System.out.println("#######################################");
+            System.out.println("List For Specific Char");
             dict.printSpecificChar('a');
+            dict.printSpecificChar('g');
         } else {
             System.out.println("Please enter some strings First ");
         }
